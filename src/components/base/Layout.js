2fac9ef3cc793815f86c1a4fbf7271e1ac3989d0
usmanghulam <usmanghulam01@gmail.com>
2022-11-30 21:11:40 +0500

    done

--- src/components/base/Layout.js
+++ src/components/base/Layout.js
@@ -7,7 +7,7 @@ const Layout = ({ children }) => {
 	return <>
 		<nav>
 			<div className='logo'><Logo /></div>
-			<div><p>New Questionnaire</p></div>
+			<div contentEditable={true}><p>New Questionnaire</p></div>
 			<div><button>LOG IN</button></div>
 		</nav>
 		{children}
